29c325563ccd6f7ec7c1dd166dc9eb86ba63f319
Daniel Montes Gil <dmontesgil@gmail.com>
2022-11-29 21:08:59 -0500

    Architecture refactored, move input action to the controller for more flexibility, readability and control, implemented map instead of list, implemented OnValidate

--- Assets/Scripts/Abilities/AbilityBase.cs
+++ Assets/Scripts/Abilities/AbilityBase.cs
@@ -7,14 +7,12 @@ public abstract class AbilityBase : MonoBehaviour
     [SerializeField] private string _description;
     [SerializeField] private Sprite _icon;
     [SerializeField] private bool _isEnable;
-    [SerializeField] private InputActionReference _inputAction;
 
 
     public string AbilityName { get => _abilityName; set => _abilityName = value; }
     public string Description { get => _description; set => _description = value; }
     public Sprite Icon { get => _icon; set => _icon = value; }
     public bool IsEnable { get => _isEnable; set => _isEnable = value; }
-    public InputActionReference InputAction { get => _inputAction; set => _inputAction = value; }
 
     public abstract void UseAbility();
 
--- Assets/Scripts/Manager/AbilityController.cs
+++ Assets/Scripts/Manager/AbilityController.cs
@@ -4,50 +4,45 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class AbilityController : MonoBehaviour
 {
-    [SerializeField] private List<AbilityBase> _abilities = new List<AbilityBase>();
-    [SerializeField] private PlayerInput playerInput;
-    private readonly Dictionary<Guid, AbilityBase> _abilitiesMap = 
-        new Dictionary<Guid, AbilityBase>();
-
-
-    public void AddAbility(AbilityBase ability)
+    [Serializable]
+    public struct AbilityMap
     {
-        _abilitiesMap.Add(ability.InputAction.action.id, ability);
-    }
-
-    public void RemoveAbility(AbilityBase ability)
-    {
-        _abilitiesMap.Remove(ability.InputAction.action.id);
+        public AbilityBase _ability;
+        public InputActionReference _abilityInput;
     }
     
+    [SerializeField] private List<AbilityMap> _abilities = new List<AbilityMap>();
+    [SerializeField] private PlayerInput playerInput;
+
     public void ToggleAbilities(bool toggle)
     {
-        foreach (var (key, value) in _abilitiesMap)
+        foreach (var abilityMap in _abilities)
         {
-            value.enabled = toggle;
+            abilityMap._ability.enabled = toggle;
         }
     }
 
     public void ToggleAbility(string abilityName, bool toggle)
     {
-        foreach (var value in _abilitiesMap.Values)
+        foreach (var abilityMap in _abilities)
         {
-            if (value.name == abilityName)
+            if (abilityMap._ability.name == abilityName)
             {
-                value.enabled = toggle;
+                abilityMap._ability.enabled = toggle;
             }
         }
     }
 
     public AbilityBase GetAbility(Guid abilityId)
     {
-        foreach (var (key,value) in _abilitiesMap)
+        foreach (var abilityMap in _abilities)
         {
-            if (key == abilityId)
+            if (abilityMap._abilityInput.action.id == abilityId)
             {
-                return value;
+                return abilityMap._ability;
             }
         }
 
@@ -76,7 +71,39 @@ public class AbilityController : MonoBehaviour
     {
         foreach (var ability in _abilities)
         {
-            AddAbility(ability);
+            foreach (var actionEvent in playerInput.actionEvents)
+            {
+                if (ability._abilityInput.action.id.ToString() == actionEvent.actionId)
+                {
+                    actionEvent.AddListener(UseAbility);
+                }
+            }
+        }
+    }
+
+    private void OnValidate()
+    {
+        for (int i = 0; i < _abilities.Count; i++)
+        {
+            for (int j = 0; j < _abilities.Count; j++)
+            {
+                if(i==j)
+                {
+                    continue;
+                }
+
+                if (!_abilities[j]._ability || !_abilities[j]._abilityInput)
+                {
+                    Debug.LogError("You have some ability unassigned or a null InputAction at index: " + j);
+                    return;
+                }
+
+                if (_abilities[i]._abilityInput.action.id == _abilities[j]._abilityInput.action.id)
+                {
+                    Debug.LogError("You can't assign same input to different abilities. Check InputAction in each ability");
+                    return;
+                }
+            }
         }
     }
 }
