572b6967c5cc607faa01d126579e9020555f231d
Cristian <crist.1707@hotmail.com>
2023-03-02 18:01:04 -0500

    started with cost

--- Assets/Scripts/Abilities/Garen Abilities/WDeGaren.cs
+++ Assets/Scripts/Abilities/Garen Abilities/WDeGaren.cs
@@ -1,15 +1,41 @@
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class WDeGaren : AbilityBase, IActivable
+public class WDeGaren : AbilityBase, IActivable, ICostable
 {
     [SerializeField] private InputActionReference _abilityInput;
 
     public InputActionReference AbilityInput => _abilityInput;
+    
+    public float Cost { get; }
+    public CostType CostType { get; }
+    public bool CheckCanUse()
+    {
+        Stat outStat = new Stat();
+        if (FindStat("Mana", ref outStat))
+        {
+            if (outStat.value > Cost)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        return false;
+    }
 
     public override void Setup(Stats stats)
     {
+        Stat manaStat = stats.GetStatByID("Mana");
+        if (string.IsNullOrEmpty(manaStat.statId))
+        {
+            Debug.LogError("Mana stat doesnt exist");
+            return;
+        }
         
+        impactedStats.Add(manaStat);
     }
 
     public override void Ability()
@@ -17,4 +43,5 @@ public class WDeGaren : AbilityBase, IActivable
         Debug.LogError("Trigger W de garen");
 
     }
+
 }
--- Assets/Scripts/Interfaces/ICostable.cs
+++ Assets/Scripts/Interfaces/ICostable.cs
@@ -1,4 +1,16 @@
+using System;
+
 public interface ICostable
 {
     float Cost { get; }
+    CostType CostType { get; }
+
+    public bool CheckCanUse();
+
+}
+
+[Serializable]
+public enum CostType
+{
+    Mana = 0,
 }
