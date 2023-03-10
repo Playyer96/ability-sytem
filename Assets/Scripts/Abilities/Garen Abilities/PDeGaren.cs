c5012c2b9e9a01000d6ddd1b31aa5cf10b76ca12
Daniel Montes Gil <dmontesgil@gmail.com>
2023-03-09 19:41:43 -0500

    Revert "Change name to appropriate names to show"
    This reverts commit 6bf34f3c5637c9b7650767e71299662227ff8e45.

--- Assets/Scripts/Abilities/Garen Abilities/PDeGaren.cs
+++ Assets/Scripts/Abilities/Garen Abilities/PDeGaren.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PasiveTest : AbilityBase, ITickeable
+public class PDeGaren : AbilityBase, ITickeable
 {
     public event Action<ITickeable> OnActiveTick;
     public event Action<ITickeable> OnDisableTick;
